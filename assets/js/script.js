5196b597e3091dffe6c6d46f386ed518601a9df4
steveforrest <steveforrest_1@hotmail.com>
2021-12-20 10:03:44 +0000

    finished interview time, added event handlers instead of onclicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,17 @@ let a2 = document.getElementById(a2);
 let a3 = document.getElementById(a3);
 let a4 = document.getElementById(a4);
 
-function a1.answer(){
+document.getElementById("a1").addEventListener("click", wrongAnswer);
+document.getElementById("a2").addEventListener("click", wrongAnswer);
+document.getElementById("a3").addEventListener("click", correctAnswer);
+document.getElementById("a4").addEventListener("click", wrongAnswer);
+
+function wrongAnswer(){
     button.style.background = 'red';
     let message = document.getElementById("message").innerHTML = "Incorrect!";
 }
 
-function a2.answer(){
-    button.style.background = 'red';
-    document.getElementById("p1").innerHTML = "Incorrect!";
-}
-
-function a3.answer(){
+function correctAnswer(){
     button.style.background = 'green';
     let message = document.getElementById("message").innerHTML = "Correct!";
-}
-
-function a4.answer(){
-    button.style.background = 'red';
-    let message = document.getElementById("message").innerHTML = "Incorrect!";
 }
