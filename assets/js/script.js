a4c0207b27614332b188522bab0cd5173dc71903
steveforrest <steveforrest_1@hotmail.com>
2021-12-20 09:46:17 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+let a1 = document.getElementById(a1);
+let a2 = document.getElementById(a2);
+let a3 = document.getElementById(a3);
+let a4 = document.getElementById(a4);
+
+function a1.answer(){
+    button.style.background = 'red';
+    let message = document.getElementById("message").innerHTML = "Incorrect!";
+}
+
+function a2.answer(){
+    button.style.background = 'red';
+    document.getElementById("p1").innerHTML = "Incorrect!";
+}
+
+function a3.answer(){
+    button.style.background = 'green';
+    let message = document.getElementById("message").innerHTML = "Correct!";
+}
+
+function a4.answer(){
+    button.style.background = 'red';
+    let message = document.getElementById("message").innerHTML = "Incorrect!";
+}
