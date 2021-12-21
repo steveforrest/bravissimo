8a3afe720c36350e2d56f0df24edc1ba013af400
steveforrest <steveforrest_1@hotmail.com>
2021-12-21 12:42:55 +0000

    completed task 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
-let a1 = document.getElementById(a1);
-let a2 = document.getElementById(a2);
-let a3 = document.getElementById(a3);
-let a4 = document.getElementById(a4);
+let a1 = document.getElementById('a1');
+let a2 = document.getElementById('a2');
+let a3 = document.getElementById('a3');
+let a4 = document.getElementById('a4');
 
-document.getElementById("a1").addEventListener("click", wrongAnswer);
+a1.addEventListener("click", wrongAnswer);
 document.getElementById("a2").addEventListener("click", wrongAnswer);
 document.getElementById("a3").addEventListener("click", correctAnswer);
 document.getElementById("a4").addEventListener("click", wrongAnswer);
 
-function wrongAnswer(){
-    button.style.background = 'red';
-    let message = document.getElementById("message").innerHTML = "Incorrect!";
+function wrongAnswer(e){
+    e.preventDefault();
+    if(e.target === a1 || a2 ||a4){
+        e.target.style.background = 'red';
+    }
+       let message = document.getElementById("message").innerHTML = "Incorrect!";
 }
 
-function correctAnswer(){
-    button.style.background = 'green';
+function correctAnswer(e){
+    e.preventDefault();
+    if(e.target === a3){
+        e.target.style.background = 'green';
+    }
     let message = document.getElementById("message").innerHTML = "Correct!";
 }
+
