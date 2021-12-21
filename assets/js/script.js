21558ba7441449f83af05d231385a89b641ba641
steveforrest <steveforrest_1@hotmail.com>
2021-12-21 16:02:16 +0000

    question 2 is now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,37 @@
-let a1 = document.getElementById('a1');
-let a2 = document.getElementById('a2');
-let a3 = document.getElementById('a3');
-let a4 = document.getElementById('a4');
-
 a1.addEventListener("click", wrongAnswer);
 document.getElementById("a2").addEventListener("click", wrongAnswer);
 document.getElementById("a3").addEventListener("click", correctAnswer);
 document.getElementById("a4").addEventListener("click", wrongAnswer);
+document.getElementById("check").addEventListener("click", question2);
+
 
 function wrongAnswer(e){
     e.preventDefault();
+    const a1 = document.getElementById('a1');
+    const a2 = document.getElementById('a2');
+    const a4 = document.getElementById('a4');
     if(e.target === a1 || a2 ||a4){
         e.target.style.background = 'red';
     }
-       let message = document.getElementById("message").innerHTML = "Incorrect!";
+       const message = document.getElementById("message1").innerHTML = "Incorrect!";
 }
 
 function correctAnswer(e){
     e.preventDefault();
+    const a3 = document.getElementById('a3');
     if(e.target === a3){
         e.target.style.background = 'green';
     }
-    let message = document.getElementById("message").innerHTML = "Correct!";
+    const message = document.getElementById("message1").innerHTML = "Correct!";
 }
 
+function question2(e){
+    e.preventDefault();
+    const answer2 = document.getElementById("q2").value;
+    const expected = '1995';
+    if(answer2 === expected){
+        const message = document.getElementById("message2").innerHTML = "Correct!";
+    }else{
+        const message = document.getElementById("message2").innerHTML = "Incorrect!";
+    }
+}
